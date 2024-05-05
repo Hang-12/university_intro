4ae0da42b66e9b615ac466e858d377cdb33885b7
Hang <sahzonghang@gmail.com>
2024-05-06 00:33:11 +0800

    first commit

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import './Contact.css'
+import msg_icon from '../../assets/msg-icon.png'
+import mail_icon from '../../assets/mail-icon.png'
+import phone_icon from '../../assets/phone-icon.png'
+import location_icon from '../../assets/location-icon.png'
+import white_arrow from '../../assets/white-arrow.png'
+
+const Contact = () => {
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "91b98760-5571-41bb-9b12-1d8c75cab01c");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
+  return (
+    <div className='contact'>
+      <div className="contact-col">
+        <h3>Send us a message <img src={msg_icon} alt="" /></h3>
+        <p>Feel free to reach out through contact form or find our contact information below.</p>
+        <ul>
+          <li><img src={mail_icon} alt="" />Contact</li>
+          <li><img src={phone_icon} alt="" />+1 123-444-7845</li>
+          <li><img src={location_icon} alt="" />Contact@gmail.com</li>
+        </ul>
+      </div>
+      <div className="contact-col">
+        <form onSubmit={onSubmit}>
+          <label htmlFor="">Your name</label>
+          <input type="text" placeholder="Name" required/>
+          <label htmlFor="">Your phone number</label>
+          <input type="tel" placeholder="Phone Number" required/>
+          <label htmlFor="">Write your messages here</label>
+          <textarea rows="6" placeholder="Message" required></textarea>
+          <button type='submit' className='btn dark-btn'>Submit Now <img src={white_arrow} alt="" /></button>
+        </form>
+        <span>{result}</span>
+      </div>
+    </div>
+  )
+}
+
+export default Contact
